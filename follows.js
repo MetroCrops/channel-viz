8de06e07acf6fa51b8e96db81ab0f2b9f5b4bf35
MetroCrops <MetroCrops@users.noreply.github.com>
2014-06-25 15:52:58 -0400

    Replaced My changes to put back to original...

--- follows.js
+++ follows.js
@@ -21,7 +21,15 @@
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
-
+	// URL Parameters
+	function getParam(key) {
+	 	var value = location.hash.match(new RegExp(key+'=([^&]*)'));
+		if(value) {
+			return value[1];
+		} else {
+			return "";
+		}
+	}
 
 	// Graph Annotations
 	function addAnnotation(force) {
@@ -125,8 +133,61 @@
 									});
 
 									// Add Datapoints Array to Graph Series Array
-									
-								
+									series.push({
+										name: datastream.id,
+										data: points,
+										color: '#' + dataColor
+									});
+
+									// Initialize Graph DOM Element
+									$('#feed-' + feedId + ' .datastreams .datastream-' + datastream.id + ' .graph').attr('id', 'graph-' + feedId + '-' + datastream.id);
+
+						 			// Build Graph
+									var graph = new Rickshaw.Graph( {
+										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
+										width: 600,
+										height: 200,
+										renderer: 'line',
+										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										padding: {
+											top: 0.02,
+											right: 0.02,
+											bottom: 0.02,
+											left: 0.02
+										},
+										series: series
+									});
+
+									graph.render();
+
+									var ticksTreatment = 'glow';
+
+									// Define and Render X Axis (Time Values)
+									var xAxis = new Rickshaw.Graph.Axis.Time( {
+										graph: graph,
+										ticksTreatment: ticksTreatment
+									});
+									xAxis.render();
+
+									// Define and Render Y Axis (Datastream Values)
+									var yAxis = new Rickshaw.Graph.Axis.Y( {
+										graph: graph,
+										tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+										ticksTreatment: ticksTreatment
+									});
+									yAxis.render();
+
+									// Enable Datapoint Hover Values
+									var hoverDetail = new Rickshaw.Graph.HoverDetail({
+										graph: graph,
+										formatter: function(series, x, y) {
+											var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + ' padding: 4px;"></span>';
+											var content = swatch + "&nbsp;&nbsp;" + parseFloat(y) + '&nbsp;&nbsp;<br>';
+											return content;
+										}
+									});
+
 									$('#feed-' + feedId + ' .datastreams .datastream-' + datastream.id + ' .slider').prop('id', 'slider-' + feedId + '-' + datastream.id);
 									var slider = new Rickshaw.Graph.RangeSlider({
 	            	   					graph: graph,
@@ -199,6 +260,66 @@
 						$('#feed-' + data.id + ' .tags').addClass('hidden');
 					}
 
+					// Location
+					if(data.location) {
+						if(data.location.name || data.location.lat || data.location.ele || data.location.disposition) {
+
+							// Location Name
+							if(data.location.name) {
+								$('#feed-' + data.id + ' .location-name .value').html(data.location.name);
+							} else {
+								$('#feed-' + data.id + ' .location-name').addClass('hidden');
+							}
+
+							// Location Coordinates
+							if(data.location.lat && data.location.lon) {
+								$('#feed-' + data.id + ' .latitude .value').html(data.location.lat);
+								$('#feed-' + data.id + ' .longitude .value').html(data.location.lon);
+							} else {
+								$('#feed-' + data.id + ' .latitude').addClass('hidden');
+								$('#feed-' + data.id + ' .longitude').addClass('hidden');
+							}
+
+							// Location Elevation
+							if(data.location.ele) {
+								$('#feed-' + data.id + ' .elevation .value').html(data.location.ele);
+							} else {
+								$('#feed-' + data.id + ' .elevation').addClass('hidden');
+							}
+
+							// Location Disposition
+							if(data.location.disposition) {
+								$('#feed-' + data.id + ' .disposition .value').html(data.location.disposition);
+							} else {
+								$('#feed-' + data.id + ' .disposition').addClass('hidden');
+							}
+
+							// Location Map
+							if(data.location.lat && data.location.lon) {
+								$('#feed-' + data.id + ' .map .value').html('<a href="http://maps.google.com/maps?z=14&t=m&q=loc:' + data.location.lat + '+' + data.location.lon + '">View on Google Maps &raquo;</a>');
+							} else {
+								$('#feed-' + data.id + ' .map').addClass('hidden');
+							}
+						} else {
+							// Location Information Unavailable
+							$('#feed-' + data.id + ' .no-location').removeClass('hidden');
+								$('#feed-' + data.id + ' .location-name').addClass('hidden');
+								$('#feed-' + data.id + ' .latitude').addClass('hidden');
+								$('#feed-' + data.id + ' .longitude').addClass('hidden');
+								$('#feed-' + data.id + ' .elevation').addClass('hidden');
+								$('#feed-' + data.id + ' .disposition').addClass('hidden');
+								$('#feed-' + data.id + ' .map').addClass('hidden');
+						}
+					} else {
+						// Location Information Unavailable
+						$('#feed-' + data.id + ' .no-location').removeClass('hidden');
+							$('#feed-' + data.id + ' .location-name').addClass('hidden');
+							$('#feed-' + data.id + ' .latitude').addClass('hidden');
+							$('#feed-' + data.id + ' .longitude').addClass('hidden');
+							$('#feed-' + data.id + ' .elevation').addClass('hidden');
+							$('#feed-' + data.id + ' .disposition').addClass('hidden');
+							$('#feed-' + data.id + ' .map').addClass('hidden');
+					}
 
 					$('#feed-' + data.id + ' .duration-hour').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
@@ -255,6 +376,8 @@
 	var yesterday = new Date(today.getTime()-1000*60*60*24*1);
 	var lastWeek = new Date(today.getTime()-1000*60*60*24*7);
 
+	var key = getParam('key');
+	var feedString = getParam('feeds');
 
 	// Check for Default Values
 	if(key == '' && defaultKey != '') {
@@ -322,8 +445,10 @@
 	$('#setFeeds').click(function() {
 		setApiKey($('#apiKeyInput').val());
 		feeds = $('#feedsInput').val().replace(/\s+/g, '').split(',');
+		window.location = './index.html#key=' + $('#apiKeyInput').val() + '&feeds=' + $('#feedsInput').val();
 		return false;
 	});
 // END Initialization
 
 })( jQuery );
+
